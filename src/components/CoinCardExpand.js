772b3ebc199aa640b1ec84b53a0297ce21d345ef
PikeyAU <66731279+PikeyAU@users.noreply.github.com>
2023-02-16 17:51:31 +1100

    Fixed delete hovering, and added hover to items

--- src/components/CoinCardExpand.js
+++ src/components/CoinCardExpand.js
@@ -1,6 +1,6 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState} from 'react'
 import { BsTrash } from 'react-icons/bs'
-import { AiOutlineEdit, AiOutlineSave } from 'react-icons/ai'
+import { AiOutlineEdit, AiOutlineSave, AiOutlineArrowLeft, AiOutlineClose } from 'react-icons/ai'
 
 import axios from 'axios'
 
@@ -9,22 +9,24 @@ const CoinCardExpand = (props) => {
     const [error, setError] = useState(null);
     const [isHover, setIsHover] = useState(false);
     const [transactionLength , setTransactionLength] = useState(0);
-    const [isHoverTrash, setIsHoverTrash] = useState(false);
     const [editing, setEditing] = useState({});
     const [amount, setAmount] = useState(0);
     const [buyPrice, setBuyPrice] = useState(0);
     const [buyDate, setBuyDate] = useState(new Date());
+    const [editHover, setEditHover] = useState({});
+    const [trashHover, setTrashHover] = useState({});
+    const [cancelHover, setCancelHover] = useState({});
 
     const clicked = props.clicked
 
     const cardStyles = {
 
         display: 'grid',
-        gridTemplateColumns: '1fr 1fr 1fr 1fr 1fr 0.25fr',
+        gridTemplateColumns: '1fr 1fr 1fr 1fr 1fr 0.25fr 0.25fr',
         justifyContent: 'center',
         color: 'white',
-        border: isHover ? 'solid #DB7D13 1px' : 'solid white 1px',
-        boxShadow: isHover ? '0 0 10px #DB7D13' : 'none',
+        border: 'solid white 1px',
+        
         height: props.transactions.length > 1 ? (transactionLength + 'vh') : '5vh',
         width: '25vw',
         margin: 'auto',
@@ -35,13 +37,14 @@ const CoinCardExpand = (props) => {
         backgroundColor:'#282C34',
         transition: 'all 0.3s ease-in-out',
         position: 'relative',
+        zIndex: 1,
         
     }
 
     const cardStyles2 = {
         display: 'flex',
         width: '100%',
-        gridTemplateColumns: '1fr 1fr 1fr 1fr 1fr 1fr',
+        gridTemplateColumns: '1fr 1fr 1fr 1fr 1fr 1fr 1fr',
         alignItems: 'center',
         color: 'white',
         //box shadow when hovering black and white
@@ -50,20 +53,24 @@ const CoinCardExpand = (props) => {
         
     }
 
+    const backArrowStyles = {
+        //Centre div
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        //rounded corners
+        color: 'white',
+        cursor: 'pointer',
+
+    }
+
+
     function handleHover() {
         setIsHover(true)
     }
 
     function handleHoverLeave() {
         setIsHover(false)
-    }
-
-    function handleDeleteHover() {
-        setIsHoverTrash(true)
-    }
-
-    function handleDeleteHoverLeave() {
-        setIsHoverTrash(false)
     }
 
     const handleEdit = (id) => {
@@ -75,7 +82,38 @@ const CoinCardExpand = (props) => {
         editHolding(id)
     }
 
+    const handleCancelEdit = (id) => {
+        setEditing({ ...editing, [id]: false})
+    }
 
+    const handleEditHover = (id) => {
+        setEditHover((prevState) => ({ ...prevState, [id]: true}))
+        
+    }
+
+    const handleEditHoverLeave = (id) => {
+        setEditHover((prevState) => ({ ...prevState, [id]: false}))
+    }
+
+    const handleTrashHover = (id) => {
+        setTrashHover({...trashHover, [id]: true})
+
+    }
+
+    const handleTrashHoverLeave = (id) => {
+        setTrashHover({...trashHover, [id]: false})
+        
+    }
+
+    const handleCancelHover = (id) => {
+        setCancelHover({...cancelHover, [id]: true})
+    }
+
+    const handleCancelHoverLeave = (id) => {
+        setCancelHover({...cancelHover, [id]: false})
+    }
+
+    
     function deleteHolding(id) {
         const response = axios.post('http://localhost:8000/api/user/delete/holding/', {id: id},
 
@@ -103,6 +141,11 @@ const CoinCardExpand = (props) => {
         )
     }
 
+    const handleContainerClick = () => {
+        props.setClicked(!clicked)
+
+    }
+
 
 
     useEffect(() => {
@@ -112,8 +155,8 @@ const CoinCardExpand = (props) => {
     
 
         return (
-            
-            <div style={cardStyles} onClick = {() => props.setClicked(!props.clicked)} onMouseEnter = {handleHover} onMouseLeave = {handleHoverLeave}> 
+            <div>
+            <div id = 'card' style={cardStyles} onMouseEnter = {handleHover} onMouseLeave = {handleHoverLeave}> 
                 <div style = {{gridColumnStart: 1, gridColumnEnd: 2, marginLeft: '6%'}}>Date</div>
                 <div style = {{gridColumnStart: 2, gridColumnEnd: 3, marginLeft: '0%'}}>Quantity</div>
                 <div style = {{gridColumnStart: 3, gridColumnEnd: 4, marginLeft: '0%'}}>Price $</div>
@@ -122,6 +165,10 @@ const CoinCardExpand = (props) => {
 
             {props.transactions.map((transaction, index)  => {
                 const isEditing = editing[transaction.id]
+                const isEditHover = editHover[transaction.id]
+                const isHoverTrash = trashHover[transaction.id]
+                const isHoverCancel = cancelHover[transaction.id]
+                
                 return (
                     <div key = {transaction.id} style = {{...cardStyles2, top: `${index * 35}px`, marginTop: '5%', marginLeft: '1.5%' }} >
                         <div style = {{gridColumnStart: 1, gridColumnEnd: 2, width: '19%'}}>{transaction.coin_buy_date}</div>
@@ -130,14 +177,17 @@ const CoinCardExpand = (props) => {
                         <div style = {{gridColumnStart: 3, gridColumnEnd: 4, width: '19%'}}>{transaction.coin_buy_price}</div>
                         <div style = {{gridColumnStart: 4, gridColumnEnd: 5, width: '19%'}}>{transaction.coin_quantity * transaction.coin_buy_price}</div>
                         <div style = {{gridColumnStart: 5, gridColumnEnd: 6, width: '19%', color: transaction.coin_buy_price < props.currentPrice ? 'green' : 'red'}}>{((transaction.coin_quantity * (props.currentPrice - transaction.coin_buy_price)) / (transaction.coin_quantity * transaction.coin_buy_price) * 100).toFixed(2)}%</div>
-                        <div style= {{marginRight: '3%', cursor: 'pointer', color: isHoverTrash ? '#DB7D13' : 'white'}}><BsTrash onMouseEnter={handleDeleteHover} onMouseLeave = {handleDeleteHoverLeave} onClick = {() => deleteHolding(transaction.id)}/></div>
-                        {isEditing ? <div style= {{marginRight: '3%', cursor: 'pointer'}}><AiOutlineSave onClick = {() => handleSave(transaction.id)} /></div>
-                        : <div style= {{marginRight: '3%', cursor: 'pointer'}} ><AiOutlineEdit onClick = {() => handleEdit(transaction.id)}/></div>}
+                        {isEditing ? <div id = 'cancel' style= {{marginRight: '3%', cursor: 'pointer', color: isHoverCancel ? '#DB7D13' : 'white'}} onMouseEnter ={() => handleCancelHover(transaction.id)} onMouseLeave={() => handleCancelHoverLeave(transaction.id)}><AiOutlineClose onClick = {() => handleCancelEdit(transaction.id)}/></div> :
+                        <div id = 'delete' style= {{marginRight: '3%', cursor: 'pointer', color: isHoverTrash ? '#DB7D13' : 'white'}} onMouseEnter ={() => handleTrashHover(transaction.id)} onMouseLeave={() => handleTrashHoverLeave(transaction.id)}><BsTrash onClick = {() => deleteHolding(transaction.id)}/></div>}
+                        {isEditing ? <div id = 'save' style= {{marginRight: '3%', cursor: 'pointer', color: isEditHover ? '#DB7D13' : 'white' }} onMouseEnter ={() => handleEditHover(transaction.id)} onMouseLeave={() => handleEditHoverLeave(transaction.id)}><AiOutlineSave onClick = {() => handleSave(transaction.id)}/></div>
+                        : <div id = 'edit' style= {{marginRight: '3%', cursor: 'pointer', color: isEditHover ? '#DB7D13' : 'white' }} onMouseEnter ={() => handleEditHover(transaction.id)} onMouseLeave={() => handleEditHoverLeave(transaction.id)} ><AiOutlineEdit onClick = {() => handleEdit(transaction.id)}/></div>}
                         
                         
                     </div>
                 )
             })}
+            </div>
+                <div style = {backArrowStyles} onClick = {handleContainerClick}><AiOutlineArrowLeft/> Back</div>
             </div>
         
             
